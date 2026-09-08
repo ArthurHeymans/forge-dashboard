4b12708c76b3c530795a18370ff0cdf5c27d27a8
Arthur Heymans <arthur@aheymans.xyz>
2026-09-08 08:55:33 +0200

    Add melpazoid CI check and MELPA packaging metadata

--- forge-dashboard-triage.el
+++ forge-dashboard-triage.el
@@ -1,5 +1,28 @@
 ;;; forge-dashboard-triage.el --- Triage support for Forge dashboard  -*- lexical-binding: t; -*-
 
+;; Copyright (C) 2026 Arthur Heymans <arthur@aheymans.xyz>
+
+;; Author: Arthur Heymans <arthur@aheymans.xyz>
+;; Homepage: https://github.com/ArthurHeymans/forge-dashboard
+;; Keywords: tools, vc
+
+;; SPDX-License-Identifier: GPL-3.0-or-later
+
+;; This file is part of forge-dashboard.
+
+;; forge-dashboard is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; forge-dashboard is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 ;;; Commentary:
 
 ;; Local attention classification, urgency ordering, and triage state for
